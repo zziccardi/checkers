ceed9516f29df41f2b4e9affa6d8afcdcfa03169
Mark Donohue <kram4md@yahoo.com>
2014-12-13 21:55:40 -0500

    Side menu in Checkerboard

--- Checkerboard.py
+++ Checkerboard.py
@@ -10,6 +10,75 @@ class Checkerboard(Frame):
         Frame.__init__(self)
         self.master.title("Checkerboard")
         self.grid()
+
+        # New Frame for menu title
+        self.__titleFrame = Frame(self)
+        self.__titleFrame.grid(column = 9, row = 1)
+
+        # New Frame for Names
+        self.__nameFrame = Frame(self)
+        self.__nameFrame.grid(column = 9, row = 2, padx = 20)
+
+        # New Frame for Turn
+        self.__turnFrame = Frame(self)
+        self.__turnFrame.grid(column = 9, row = 3)
+
+        
+        # Label for the 'title' of the menu
+        self.__startGameLabel = Label(self.__titleFrame, \
+                                      text = "Let's play Checkers!")
+
+        # Player name labels, Player name entries,
+        # Submit buttons
+        self.__redPlayerLabel = Label(self.__nameFrame, \
+                                      text = "Red Team Player Name:")
+        self.__redNameEntry = Entry(self.__nameFrame, \
+                                    width = 10)
+        self.__redOKButton = Button(self.__nameFrame, \
+                                        text = "OK", \
+                                        command = self.__setRedName)                                         
+        self.__whitePlayerLabel = Label(self.__nameFrame, \
+                                        text = "White Team Player Name:")
+        self.__whiteNameEntry = Entry(self.__nameFrame, \
+                                      width = 10)
+        self.__whiteOKButton = Button(self.__nameFrame, \
+                                      text = "OK", \
+                                      command = self.__setWhiteName)
+
+
+        # Updating label that displays who's turn it is
+        self.__turnLabel = Label(self.__turnFrame, \
+                                 text = "Your Turn:")
+        '''Code to make the turn label change with each move'''
+        self.__turnValue = StringVar()
+        self.__displayTurn = Label(self.__turnFrame, \
+                                   textvariable = self.__turnValue)
+        
+
+        # Updating label that tells user if their move was valid or invalid
+        self.__moveAnalysisLabel = Label(self.__turnFrame, \
+                                         text = "Move Analysis:")
+        '''Code to make the move analysis label change with each move'''
+        self.__analysisValue = StringVar()
+        self.__displayAnalysis = Label(self.__turnFrame, \
+                                       textvariable = self.__analysisValue)
+
+        # Organize titleFrame
+        self.__startGameLabel.grid(   column = 0, row = 0)
+
+        # Organize nameFrame
+        self.__redPlayerLabel.grid(   column = 0, row = 1)
+        self.__redNameEntry.grid(     column = 1, row = 1)
+        self.__redOKButton.grid(      column = 2, row = 1)
+        self.__whitePlayerLabel.grid( column = 0, row = 2)
+        self.__whiteNameEntry.grid(   column = 1, row = 2)
+        self.__whiteOKButton.grid(    column = 2, row = 2)
+
+        # Organize turnFrame
+        self.__turnLabel.grid(        column = 0, row = 0)
+        self.__displayTurn.grid(      column = 1, row = 0)
+        self.__moveAnalysisLabel.grid(column = 0, row = 1)
+        self.__displayAnalysis.grid(  column = 1, row = 1)
         
         self.__redChecker = PhotoImage(file = "redChecker.gif")
         self.__whiteChecker = PhotoImage(file = "whiteChecker.gif")
@@ -171,3 +240,21 @@ class Checkerboard(Frame):
     def __activated(self, space):
         checkersEvaluation.spaceClicked(self, space)
         print(space)
+
+    def __setRedName(self):
+        redName = self.__redNameEntry.get()
+
+    def __setWhiteName(self):
+        whiteName = self.__whiteNameEntry.get()
+
+    def updateTurnValue(self):
+        if self.__turn == 1:
+            self.__turnValue.set(redName)
+        else:
+            self.__turnValue.set(whiteName)
+
+    def updateAnalysisValue(self):
+#        if checkersEvaluation.invalidMoveMessage():
+        self.__analysisValue.set("Invalid Move!")
+#        else:
+#            self.__analysisValue.set("Nice move, bro")
